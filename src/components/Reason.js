d16d112266c77b1a325ee3f525978962842170b2
ruhsari <bulatovaruhsari@gmail.com>
2025-11-04 10:55:30 +0600

    Ruhsari

--- src/components/Reason.js
+++ src/components/Reason.js
@@ -1,12 +1,12 @@
 import React from 'react'
-// import { ways } from '..data'
 
 export default function Reason(props) {
   return (
-    <div>
+    <div className='reason-card'>
       {/* <img src={icon_1} alt="icon_1" className="icon_1"></img> */}
       <h2>{props.title}</h2>
       <p>{props.description}</p>
+      <p></p>
     </div>
   )
 }
--- src/components/Reasons.js
+++ src/components/Reasons.js
@@ -1,14 +1,25 @@
 import React from 'react'
-// import { ways } from '..data'
+import { ways } from '../data'
 import Reason from './Reason'
 
 export default function Reasons() {
   return (
     <div>
-      <Reason title='111' description='222'/>
-      <Reason title='333' description='444'/>
-      <Reason title='555' description='666'/>
-      <Reason title='777' description='888'/>
+      <Reason 
+        title={ways[0].title} 
+        description={ways[0].description}
+      />
+
+      <Reason 
+        title={ways[1].title} 
+        description={ways[1].description} 
+      />
+
+      <Reason 
+        title={ways[2].title} 
+        description={ways[2].description}
+      />
+
     </div>
   )
 }
